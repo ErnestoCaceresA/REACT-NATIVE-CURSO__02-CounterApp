b9eb9d9d9c3fabc07e8e5e7eb9e939035c207000
Ernesto Caceres <ernesto.caceresay@gmail.com>
2021-06-17 16:45:23 -0500

    Primer Commit Hola mundo
    En este commit se creo la aplicacion y se organizo un poco las carpetas
    para crear la screen de Hola mundo

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+import { HolaMundoScreen } from './src/screens/HolaMundoScreen'
+
+const App = () => {
+  return (
+    <HolaMundoScreen />
+  )
+}
+
+export default App;
